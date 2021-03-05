ff96ab96e468a090b497b8242daa8be8acc77f24
Sietseq <sietse.05@gmail.com>
2021-03-05 18:06:29 -0400

    Simple demo working

---
+++ vsProject/ArgonGodot/Argon2.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Godot.hpp>
+#include <KinematicBody2D.hpp>
+#include <Input.hpp>
+
+namespace godot
+{
+	class Argon2 : public Node
+	{
+
+		// Godot structure
+	private:
+		GODOT_CLASS(Argon2, Node)
+	public:
+		static void _register_methods();
+		void _init();
+		void _process(float delta);
+
+		Argon2();
+		~Argon2();
+
+
+		// Gameplay methods
+	public:
+		void RawHash();
+
+	private:
+
+
+
+
+	};
+}
